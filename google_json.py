cfafb38bad95b366b6f057ce3c75f3e994e526a1
Rishabh Dilip Ranawat <rishabh@172-16-186-59.DYNAPOOL.NYU.EDU>
2015-10-26 13:57:47 -0400

    added try catch

--- google_json.py
+++ google_json.py
@@ -1,11 +1,11 @@
-import urllib2
+import urllib2, re
 import simplejson
 
 from clarifai.client import ClarifaiApi
 clarifai_api = ClarifaiApi()  # assumes environment variables are set.
 
 url = ('https://ajax.googleapis.com/ajax/services/search/images?' +
-       'v=1.0&q=google%20workers&userip=INSERT-USER-IP&start=8')
+       'v=1.0&q=nurse&userip=INSERT-USER-IP&start=0')
 
 request = urllib2.Request(url, None, {})
 response = urllib2.urlopen(request)
@@ -15,15 +15,22 @@ womenCounter = 0
 results = simplejson.load(response)
 
 for i in range(0, len(results), 1):
-	values = clarifai_api.tag_image_urls(results.get('responseData').get('results')[i].get('url'))
+	resp =results.get('responseData').get('results')[i].get('url')
+	
+	try:
+		values = clarifai_api.tag_image_urls(resp)
+	except:
+		pass
 	tags =  values.get('results')[0].get('result').get('tag').get('classes')
 	for each in tags:
-		if(each == "men"):
+		if(each == "men" or each == "boy" or each == "man"):
 			menCounter += 1
-		elif(each == "women"):
+		elif(each == "women" or each == "girl" or each == "woman"):
 			womenCounter += 1
 		print each
 # now have some fun with the results...
 
 print(menCounter)
 print(womenCounter)
+
+
