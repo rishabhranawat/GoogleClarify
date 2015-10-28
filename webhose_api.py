1d1759774247ed6d24223a2e0436ee30d8e709a1
Rishabh Dilip Ranawat <rishabh@172-16-69-225.DYNAPOOL.NYU.EDU>
2015-10-28 15:21:15 -0400

    changes

--- webhose_api.py
+++ webhose_api.py
@@ -1,10 +1,10 @@
 import unirest
 
-response = unirest.get("https://webhose.io/search?token=237efe51-29d7-4a3e-bddf-576cd8605ff5&format=json&size=100&q=%22girls%20code%22",
+response = unirest.get("https://webhose.io/search?token=237efe51-29d7-4a3e-bddf-576cd8605ff5&format=json&size=100&q=%22black%20tech%22",
     headers={
     "Accept": "text/plain"
     }
 )
 
-for i in range(0, 99, 1):
-	print response.body.get("posts")[i].get("thread").get("url")
+for i in range(0, 98, 1):
+	print response.body.get("posts")[i].get("thread").get("title_full")
