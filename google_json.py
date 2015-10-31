e7e7917e2f0dad0b9a74fb339d52bf639cc72a7f
Rishabh Dilip Ranawat <rishabh@172-16-186-4.DYNAPOOL.NYU.EDU>
2015-10-31 11:39:42 -0400

    Made changes

--- google_json.py
+++ google_json.py
@@ -4,33 +4,40 @@ import simplejson
 from clarifai.client import ClarifaiApi
 clarifai_api = ClarifaiApi()  # assumes environment variables are set.
 
-url = ('https://ajax.googleapis.com/ajax/services/search/images?' +
-       'v=1.0&q=MIT%20computer%20science%20students&userip=INSERT-USER-IP&start=8')
-
-request = urllib2.Request(url, None, {})
-response = urllib2.urlopen(request)
 menCounter = 0
 womenCounter = 0
-# Process the JSON string.
-results = simplejson.load(response)
 
-for i in range(0, len(results), 1):
-	resp =results.get('responseData').get('results')[i].get('url')
-	try:
-		values = clarifai_api.tag_image_urls(resp)
-	except:
-		pass
+counter = 0
+while(counter != 100):
+	if(counter%4 == 0):
+		url_string = 'https://ajax.googleapis.com/ajax/services/search/images?'+'v=1.0&q=hackathons&userip=INSERT-USER-IP&start='+str(counter)
+		print url_string
+	
+	counter +=4
+	url = (url_string)
 
-	tags =  values.get('results')[0].get('result').get('tag').get('classes')
-	for each in tags:
-		if(each == "men" or each == "boy" or each == "man"):
-			menCounter += 1
-		elif(each == "women" or each == "girl" or each == "woman"):
-			womenCounter += 1
-		print each
+	request = urllib2.Request(url, None, {})
+	response = urllib2.urlopen(request)
+
+	# Process the JSON string.
+	results = simplejson.load(response)
+
+	for i in range(0, len(results), 1):
+		try:
+			resp =results.get('responseData').get('results')[i].get('url')
+			values = clarifai_api.tag_image_urls(resp)
+			tags =  values.get('results')[0].get('result').get('tag').get('classes')
+			for each in tags:
+				if(each == "men" or each == "boy" or each == "man"):
+					menCounter += 1
+				elif(each == "women" or each == "girl" or each == "woman"):
+					womenCounter += 1
+
+		except:
+			pass
+
 
 # now have some fun with the results...
-
 print(menCounter)
 print(womenCounter)
 
--- webhose_api.py
+++ webhose_api.py
@@ -1,10 +1,10 @@
 import unirest
 
-response = unirest.get("https://webhose.io/search?token=237efe51-29d7-4a3e-bddf-576cd8605ff5&format=json&size=100&q=%22black%20tech%22",
+response = unirest.get("https://webhose.io/search?token=237efe51-29d7-4a3e-bddf-576cd8605ff5&format=json&size=1000&q=%22women%20tech%22",
     headers={
     "Accept": "text/plain"
     }
 )
 
-for i in range(0, 98, 1):
+for i in range(0, 999, 1):
 	print response.body.get("posts")[i].get("thread").get("title_full")
